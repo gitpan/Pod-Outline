ff8a42e8e2d470aece528591034edfd6264092c1
Andy Lester <andy@petdance.com>
2004-11-06 21:03:17 +0000

    Import of PETDANCE/Pod-Outline-0.01 from CPAN.
    gitpan-cpan-distribution: Pod-Outline
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PETDANCE/Pod-Outline-0.01.tar.gz
    gitpan-cpan-author:       PETDANCE
    gitpan-cpan-maturity:     released

---
+++ lib/Pod/Outline.pm
@@ -0,0 +1,44 @@
+package Pod::Outline;
+
+=head1 NAME
+
+Pod::Outline - For generating outlines of POD files
+
+=cut
+
+use warnings;
+use strict;
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+This module does nothing.
+
+=head1 AUTHOR
+
+Andy Lester, C<< <andy@petdance.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-pod-outline@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically
+be notified of progress on your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2004 Andy Lester, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Pod::Outline
